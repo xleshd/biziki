5751391de62171cea5edbd03f43024bb978033a3
xleshd <mehmetbilgic185@gmail.com>
2026-07-24 14:24:19 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'biziki-v4';
+const FILES = ['./index.html', './manifest.webmanifest'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html'))));
+});
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.matchAll({type:'window', includeUncontrolled:true}).then(list=>{
+    for(const c of list){ if(c.url.includes(self.location.origin) && 'focus' in c) return c.focus(); }
+    if(clients.openWindow) return clients.openWindow('./');
+  }));
+});
